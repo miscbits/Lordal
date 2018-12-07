52b5e466ae82caf0a53a14a2a75f9ed4b5ad98d9
miscbits <willow6786@gmail.com>
2018-12-07 11:04:22 -0500

    added routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return "api is up";
 });
+
+Route::post('login/github/callback', 'Auth\LoginController@handleStudentProviderCallback');
+Route::post('login/google/callback', 'Auth\LoginController@handleStaffProviderCallback');
+
+Route::apiResource('assessments', 'AssessmentsController');
+Route::apiResource('comments', 'CommentsController');
+Route::apiResource('submissions', 'SubmissionsController');
+Route::apiResource('assignments', 'AssignmentsController');
+Route::apiResource('students', 'StudentsController');
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('login/github', 'Auth\LoginController@redirectToStudentProvider');
-Route::get('login/github/callback', 'Auth\LoginController@handleStudentProviderCallback');
-Route::get('login/google', 'Auth\LoginController@redirectToStaffProvider');
-Route::get('login/google/callback', 'Auth\LoginController@handleStaffProviderCallback');
 
