6e7c94cd49676a4aa40140f9cb0000bf8f45f4cf
miscbits <willow6786@gmail.com>
2019-01-24 14:45:11 -0500

    removed reference to non-existent table

--- database/migrations/2018_12_06_152839_create_submissions_table.php
+++ database/migrations/2018_12_06_152839_create_submissions_table.php
@@ -19,7 +19,6 @@ class CreateSubmissionsTable extends Migration
             $table->string('latest_hash')->nullable();
             $table->integer('grade')->nullable();
             $table->integer('assignment_id')->unsigned();
-            $table->foreign('assignment_id')->references('id')->on('assignments');
             $table->unique('assignment_id');
             $table->timestamps();
         });
